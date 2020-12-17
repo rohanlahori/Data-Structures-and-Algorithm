29e8f4ec8df947ad07205d3c88e314914642c399
Vaibhav Dipesh Parikh <56918464+vaibhav720@users.noreply.github.com>
2020-12-17 23:28:03 +0530

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vl vector<ll>
+#define pi pair<ll,ll>
+#define vp vector<pi>
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rall(x) x.rbegin(), x.rend()
+#define watch(x) cout << (#x) << ' is ' << (x) << endl
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define deb(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define mll map<ll,ll>
+#define mod 1000000007
+//  for (auto &a : v)
+//cin >> a;
+//for (auto me : m) {
+//std::vector<ll>::iterator it; 
+//it = std::find (v.begin(), v.end(), ser);
+//if (it != v.end())
+
+int main()
+{
+//   freopen('input.txt', 'r', stdin);
+//   freopen('output.txt', 'w', stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t,n;
+    cin>>t;
+    while(t--)
+    {
+        ll a,co=0,c=0,d=0;
+        cin>>n;
+        ll aa[n];
+        vector<ll> v;
+        map<ll,ll> m;
+        f(j,0,n)
+        {
+            cin>>aa[j];
+        }
+    }
+   return 0;
+}
