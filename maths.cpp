93b5ffe61d5e18c533ae461728610685a210d0c8
Vaibhav Dipesh Parikh <56918464+vaibhav720@users.noreply.github.com>
2020-12-18 21:33:38 +0530

    Add files via upload

---
+++ maths.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vl vector<ll>
+#define pi pair<ll,ll>
+#define vp vector<pi>
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rall(x) x.rbegin(), x.rend()
+#define watch(x) cout << (#x) << ' is ' << (x) << endl
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define deb(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define mll map<ll,ll>
+#define mod 1000000007
+//  for (auto &a : v)
+//cin >> a;
+//for (auto me : m) {
+//std::vector<ll>::iterator it; 
+//it = std::find (v.begin(), v.end(), ser);
+//if (it != v.end())
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int n;
+vector<char> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i <= n; i++) {
+    if (is_prime[i] && (long long)i * i <= n) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}
+int main()
+{
+//   freopen('input.txt', 'r', stdin);
+//   freopen('output.txt', 'w', stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t,n;
+    cin>>t;
+    while(t--)
+    {
+        ll co=0;
+        f(i,0,9)
+        {
+            string aa;
+            cin>>aa;
+            if(aa[i]=='1')
+            {
+                aa[i]='2';
+            }
+            else
+            {
+                aa[i]='1';
+            }
+             cout<<aa<<"\n";
+        }
+       
+    }
+   return 0;
+}
