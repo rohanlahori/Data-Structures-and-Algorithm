6e3c9404e643c8706aff4c4f733067daef4e058b
Vaibhav Dipesh Parikh <56918464+vaibhav720@users.noreply.github.com>
2020-12-19 19:31:00 +0530

    Add files via upload

---
+++ sum_of_difference.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vl vector<ll>
+#define pi pair<ll,ll>
+#define vp vector<pi>
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rall(x) x.rbegin(), x.rend()
+#define watch(x) cout << (#x) << ' is ' << (x) << endl
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define deb(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define mll map<ll,ll>
+#define mod 1000000007
+//  for (auto &a : v)
+//cin >> a;
+//for (auto me : m) {
+//std::vector<ll>::iterator it; 
+//it = std::find (v.begin(), v.end(), ser);
+//if (it != v.end())
+
+int main()
+{
+//   freopen('input.txt', 'r', stdin);
+//   freopen('output.txt', 'w', stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t,n;
+    cin>>t;
+    vector<ll> v(t);
+    ll a,co=0,c=0,d=0;
+    f(i,0,t)
+    {
+        cin>>v[i];
+    }
+    srt(v);
+    f(i,1,t)
+    {
+        co+=(v[i]-v[i-1])*(t-i)*i;
+        
+    }
+    cout<<co;
+   return 0;
+}
