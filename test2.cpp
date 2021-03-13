1eeed9c306b29a0f1a958f801199193dc57d5cc6
Vaibhav Dipesh Parikh <56918464+vaibhav720@users.noreply.github.com>
2021-03-13 12:03:23 +0530

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -2,23 +2,30 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+//#pragma GCC optimize('O2') 
 #define ll long long 
-#define all(x) (x).begin(),(x).end()
 #define vll vector<ll>
-#define vl vector<ll>
+#define vl vector<int>
+#define tin tuple<ll,ll,ll>
+#define mll map<ll,ll>
 #define pi pair<ll,ll>
 #define vp vector<pi>
 #define pb push_back
 #define mp make_pair
 #define fi first
 #define se second
+#define mt make_tuple
+#define all(x) (x).begin(),(x).end()
 #define rall(x) x.rbegin(), x.rend()
 #define watch(x) cout << (#x) << ' is ' << (x) << endl
 #define f(i,x,z) for(ll i=x;i<z;i++)
+#define rf(i,x,z) for(ll i=x-1;i>=z;i--)
 #define deb(x) cout<<'DEBUG------> '<<x<<endl
 #define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
 #define clr(x) memset(x, 0, sizeof(x)) 
-#define mll map<ll,ll>
+#define fa(i,vect) for(auto i : vect)
+#define fr(i,vect) for(VI :: reverse_iterator i = vect.rbegin(); i != vect.rend(); i++)
 #define mod 1000000007
 //  for (auto &a : v)
 //cin >> a;
@@ -26,26 +33,27 @@ using namespace std;
 //std::vector<ll>::iterator it; 
 //it = std::find (v.begin(), v.end(), ser);
 //if (it != v.end())
+////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-//   freopen('input.txt', 'r', stdin);
-//   freopen('output.txt', 'w', stdout);
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    ll t,n;
-    cin>>t;
-    while(t--)
-    {
-        ll a,co=0,c=0,d=0;
-        cin>>n;
-        ll aa[n];
-        vector<ll> v;
-        map<ll,ll> m;
-        f(i,0,n)
-        {
-            cin>>aa[i];
-        }
-    }
+//   freopen('a.txt', 'r', stdin); freopen('a_out.txt', 'w', stdout);
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	ll t,n;
+	cin>>t;
+	while(t--)
+	{
+		ll a,co=0,c=0,d=0;
+		cin>>n;
+		ll aa[n];
+		vector<ll> v;
+		map<ll,ll> m;
+		f(j,0,n)
+		{
+			cin>>aa[j];
+		}
+	}
    return 0;
 }
